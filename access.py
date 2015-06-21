e8d7119404c736345ae38a45eb3184ddd3d2d7f7
Brandon Purvis <bcp7cc@virginia.edu>
2015-06-20 22:47:17 -0400

    Add Class to handle blame data generation.

--- access.py
+++ access.py
@@ -11,7 +11,6 @@ class Repository():
         utils.clone_repo(self.url)
         return self
 
-
     def __exit__(self, type, value, traceback):
         utils.remove_repo(self.name)
 
@@ -22,9 +21,25 @@ class Repository():
         return repr(self)
 
 
+class Blame():
+    def __init__(self, repo):
+        self.repo = repo
+    
+    def get(self):
+        file_paths = utils.traverse(self.repo.name)
+        dicts = []
+        for file_ in file_paths:
+            print(file_)
+            for line in utils.get_blame(file_):
+                dicts.append(utils.line_to_dict(line))
+        return dicts
+
+
 if __name__ == '__main__':
-    url = 'http://github.com/CenterForOpenScience/modular-file-renderer/'
+    url = 'http://github.com/CenterForOpenScience/osf.io/'
     import os
     with Repository(url) as repo:
         print(repo)
+        blame = Blame(repo)
+        print(blame.get())
         os.system('ls')
