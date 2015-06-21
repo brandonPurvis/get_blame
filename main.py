4e7e2bf09e995b4b252de68e640e2f51d0c95f1d
Brandon Purvis <bcp7cc@virginia.edu>
2015-06-21 12:24:26 -0400

    Put main programming into a function.

--- main.py
+++ main.py
@@ -6,14 +6,21 @@ import search
 FORMATS = ['txt', 'py', 'mako', 'js', 'md']
 
 
-if __name__ == '__main__':
-    url = argv[1]
-    print(url)
+def reset_index():
     search.delete_index()
     search.create_index()
+
+
+def main(repo_url):
     with access.Repository(url) as repo:
         blame = access.Blame(repo)
         for d in blame:
             if d['fname'].split('.')[-1] in FORMATS:
                 print(d)
                 search.add(d)
+
+
+if __name__ == '__main__':
+    url = argv[1]
+    print(url)
+    main(url)
